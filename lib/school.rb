20f743b9c3a185b73af21efc6e39ce4236203a3f
Kimberly Baker <bakerk2010@gmail.com>
2020-10-08 13:53:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,7 +4,9 @@ class School
   school.add_student("AC Slater", 9)
   school.add_student("Kelly Kapowski", 10)
   school.add_student("Screech", 11)
+  school.greade(9)
   school.roster
+  school.sort
   
   hash["new_key"] = []
   hash["new_key"] << "new_value_for_value_array"
